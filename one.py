44f3ece30c088b0b62bd80227d8af68eb8d291d9
UralmashFox <55876841+UralmashFox@users.noreply.github.com>
2020-03-07 00:37:12 +0300

    Update one.py

--- one.py
+++ one.py
@@ -27,7 +27,7 @@ W1_new = []
 W2_new = []
 b1_new = []
 b2_new = []
-episodes = 10
+episodes = 100
 for i_episode in range(episodes):
     W1 = np.random.randn(nhiddens,ninputs) * pvariance # first layer
     W1_all.append(W1)
@@ -58,20 +58,14 @@ for i_episode in range(episodes):
         else:
             action = np.argmax(A2)
         observation, reward, done, info = env.step(env.action_space.sample())
-        print("observation", observation, "reward", reward, "done", done, "info", info)
+        # print("observation", observation, "reward", reward, "done", done, "info", info)
         a = a+reward
     all_a.append(a)
     a = 0
-# all_a = np.reshape(np.array(all_a), episodes)
-# all_a_copy = all_a
-# all_a = np.sort(all_a)
 print(all_a)
 for i in range (int(episodes/2)):
-    # new_all = np.array(new_all_a.append(all_a[all_a.size-i-1]))
-    # count = np.where(all_a[all_a.size-i-1] == all_a)
     count = np.argmax(all_a)
-    # print(type())
-    all_a[count] = int(min(all_a))
+    all_a[count] = -10000000
     W1_new.append(W1_all[count])
     W2_new.append(W2_all[count])
     b1_new.append(b1_all[count])
@@ -85,9 +79,9 @@ W1_new = np.reshape(W1_new, (nhiddens, ninputs, episodes))
 W2_new = np.reshape(W2_new, (noutputs, nhiddens, episodes))
 b1_new = np.reshape(b1_new, (nhiddens, 1, episodes))
 b2_new = np.reshape(b2_new, (noutputs, 1, episodes))
-print ("new W1", W1_new.shape, "new W2", W2_new.shape, "new b1", b1_new.shape, "new b2", b2_new.shape)
 print(W1_new)
 print(W2_new)
 print(b1_new)
 print(b2_new)
+
 env.close()
