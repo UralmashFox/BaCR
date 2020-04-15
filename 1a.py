3feefca17b040eadfd3f277ae9c69015e4082143
UralmashFox <rikch_zurps@mail.ru>
2020-04-15 19:37:51 +0300

    folder with balancer

--- 1a.py
+++
@@ -1,7 +0,0 @@
-import gym
-env = gym.make('Humanoid-v2')
-env.reset()
-for _ in range(20000):
-    env.render()
-    observation, reward, done, info = env.step(env.action_space.sample())
-env.close()
